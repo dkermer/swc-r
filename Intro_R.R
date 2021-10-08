c646aeb54f079d8e498951affc055a8e28c62fdb
Debby Kermer <dkermer@gmail.com>
2021-10-08 16:56:32 -0400

    Add files

---
+++ Intro_R.R
@@ -0,0 +1,32 @@
+#################################
+# My Carpentries Script
+# Learning R
+##################################
+
+# Mathematical Operators ----
+
+2/1000
+
+# Logical Operators ----
+
+1 == 1
+1 != 1
+
+
+# Functions ----
+
+log(1)
+log10(1,)
+
+exp(1)
+
+log(10, base = 10)
+log(10, 10)
+log(x = 10, base = 10)
+log(base = 10, x = 10)
+
+round(22/7, digits=2)
+round
+
+install.packages("readr")
+library(readr)
